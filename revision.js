0aec9ed47c9bd11583adae7e2c9f7246d47eb306
AnkirPandey <developerankitpandey@gmail.com>
2024-05-22 08:52:25 +0530

    revision

--- revision.js
+++ revision.js
@@ -1,1 +1,20 @@
 "use strict"; //treat aal js code as newer version
+
+// ------------------------------operations-----------------------------------------
+
+// let value=4;
+// let negativeValue=-value;
+// console.log(negativeValue);
+
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+console.log(+true)
+console.log(+"");
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
