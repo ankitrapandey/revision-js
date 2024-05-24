27a43286beee77654ef5235fa1660e4d7a0921ec
AnkirPandey <developerankitpandey@gmail.com>
2024-05-24 16:32:04 +0530

    solve question

--- revision.js
+++ revision.js
@@ -7,14 +7,47 @@
 // console.log(negativeValue);
 
 
+
+
 // console.log("1"+2);
 // console.log(1+"2");
 // console.log("1"+2+2);
 // console.log(1+2+"2");
 
-console.log(+true)
-console.log(+"");
-console.log(null>0);
-console.log(null==0);
-console.log(null>=0);
 
+
+// console.log(+true)
+// console.log(+"");
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+//--------------------------- difine Symbol----------------------------
+
+// const id=Symbol('123');
+// const otherid=Symbol('123');
+// console.log(id);
+// console.log(id===otherid)
+
+
+// stack (primitive), Heap (Non-primitive)
+
+// let myName='ankit';
+// let anotherName=myName;
+// anotherName='pandey '
+// console.log(anotherName);
+
+    // let userOne={
+    //     email:'ankit@gmail.com',
+    //     upi:'user@ybl'
+    // }
+    // let userTwo=userOne
+
+    // userTwo.email='pandey@gmail.com'
+    // console.log(userOne.email);
+    // console.log(userTwo.email);
+
+
+    // use replace 
+    const url="https://ankit.com/ankit%20pandey"
+    console.log(url.replace("%20","-"));
